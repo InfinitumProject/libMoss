df50a4880ce615d489d037afdacb044b0937a2af
Universal82 <mayles06@outlook.com>
2025-04-11 19:19:07 -0400

    Fixed the read from socket connetion issue, made the client try multiple times for a connection with a pause in between on failure.

--- include/MossLib/debug.hpp
+++ include/MossLib/debug.hpp
@@ -2,10 +2,13 @@
 #include <iostream>
 #include <string.h>
 
-void dprint(const char *_input, const char *_print_if = "DEBUG"){
+void dprint(const char *_input, const char *_print_if = "DEBUG", bool _newline = true){
     const char *val = getenv(_print_if);
     bool is_true = (strcmp(val,"true") == 0) || (strcmp(val,"True") == 0);
     if (is_true){
-        std::cout << _input << std::endl;
+        std::cout << _input;
+        if (_newline){
+            std::cout << std::endl;
+        }
     }
 }
