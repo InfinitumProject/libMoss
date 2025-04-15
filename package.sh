9344761502b071e74d0a5edf3134d99a080b6aed
Moss <maylesa06@outlook.com>
2025-04-14 20:34:22 -0400

    It was broken, fixed it!

--- package.sh
+++ package.sh
@@ -19,22 +19,22 @@ mkdir -p $packageFull/usr/local/lib
 
 
 cp -f ./build/lib/libMoss.so $packageFull/lib
-cp -f ./build/lib/libMoss.so $packageFull/usr/lib
 cp -f ./build/lib/libMoss.so $packageFull/usr/local/lib
 cp -rf ./include $packageFull/usr/include/
 cp -rf ./include $packageFull/usr/local/include/
 
 
-mkdir $packageFull/DEBIAN
+mkdir $packageFull/debian
 
 echo "Package: ${packageName}
 Version: ${packageMajor}.${packageMinor}-${packageRevision}
 Section: base
-Architecture: i386
+Priority: optional
+Architecture: all
 Maintainer: Moss Mayfly <mossmayfly@gmail.com>
 Description: Miss-Moss Library
  Currently a library that I hope to expand into something greater.
- For now it is a dependecny free, C++ style TCP library." > $packageFull/DEBIAN/control
+ For now it is a dependecny free, C++ style TCP library." > $packageFull/debian/control
 
 dpkg-deb --build $packageFull
 
