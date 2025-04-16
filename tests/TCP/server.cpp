c9cc69054d4f1e3aa2c54aed7c98fa07f956d025
Universal82 <mayles06@outlook.com>
2025-04-16 10:49:57 -0400

    Made some test directory restruturing changes.

--- tests/TCP/server.cpp
+++ tests/TCP/server.cpp
@@ -1,9 +1,4 @@
 #include "../../include/MossLib/network.hpp"
-#include "../../include/MossLib/debug.hpp"
-
-#include <chrono>
-#include <functional>
-
 int server(int port) {
     using namespace std::chrono_literals;
     std::this_thread::sleep_for(2.5s);
@@ -82,30 +77,4 @@ int server(int port) {
     // Close socket
     close(server_fd);
     return 0;
-}
-
-using namespace std::chrono_literals;
-
-int main(){
-    try {
-        std::thread e(&server,1234);
-    
-        Moss::Network::TCP conn1("127.0.0.1",1234);
-        Moss::Network::TCP conn2("127.0.0.1",1234);
-    
-        conn1 << "This is a test from C1.";
-        conn1 << "EWAEWEADAS";
-        conn1 << EXIT_PACKET;
-        conn2 << "This is a test from C2.";
-        conn2 << "Ooooh.. keysmash... ABGAJSBGJBAG!!!!";
-        conn2 << EXIT_PACKET;
-    
-        std::this_thread::sleep_for(1s);
-        Moss::Network::TCP closer("127.0.0.1",1234);
-        closer << STOP_PACKET;
-        e.join();
-        return 0;
-    } catch (...) {
-        return -1;
-    }
 }
